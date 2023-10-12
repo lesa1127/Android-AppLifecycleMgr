b36a62664cbc4e3e54f7f24efd00a72faf481962
lesa1127 <1245431737@qq.com>
2023-10-12 11:15:50 +0800

    Update AppLifecyclePlugin.groovy
    .....

--- applifecycle-plugin/src/main/groovy/com/hm/iou/lifecycle/plugin/AppLifecyclePlugin.groovy
+++ applifecycle-plugin/src/main/groovy/com/hm/iou/lifecycle/plugin/AppLifecyclePlugin.groovy
@@ -1,6 +1,7 @@
 package com.hm.iou.lifecycle.plugin
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -9,8 +10,13 @@ class AppLifecyclePlugin implements Plugin<Project>{
     @Override
     void apply(Project project) {
         println "------LifeCycle plugin entrance-------"
-        def android = project.extensions.getByType(AppExtension)
-        android.registerTransform(new AppLifecycleTransform(project))
+        try {
+            def android = project.extensions.getByType(AppExtension)
+            android.registerTransform(new AppLifecycleTransform(project))
+        }catch (Throwable ignored){
+            def android = project.extensions.getByType(LibraryExtension)
+            android.registerTransform(new AppLifecycleTransform(project))
+        }
     }
 
 }
